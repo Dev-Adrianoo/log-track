2654da4df6bcf16b1d877a1997991c9db09e65e3
Dev-Adrianoo <adrianosouzaa733@gmail.com>
2025-11-12 10:44:08 -0300

    feat: configuration prisma

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,6 @@
 import express from "express"
 import Routes  from "./routes"
-
-const app = express()
-
-app.use(json.express());
-app.use('/api', Routes);
-
-app.get('/', (req, res) => {
-    res.send("Rodando!")
-})
+import app from "./app.ts"
 
 app.listen(3000, () => {
     console.log("Server rodando on: ")
